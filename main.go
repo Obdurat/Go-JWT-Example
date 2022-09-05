015dc20a1b91e6d55d65389729606937449b31a4
Anderson Rodrigues <omicron_rpl@hotmail.com>
2022-09-05 13:13:31 -0300

    Example og a goLang implementation of JWT using dgrijalva package

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var SECRET = []byte("super-secret-auth-key")
+var api_key = "1234"
+
+func CreateJWT() (string, error) {
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"name":     "Anderson",
+		"lastname": "Rodrigues",
+	})
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["exp"] = time.Now().Add(time.Hour).Unix()
+
+	tokenStr, err := token.SignedString(SECRET)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return "", err
+	}
+
+	return tokenStr, nil
+}
+
+func ValidateJWT(next func(w http.ResponseWriter, r *http.Request, t jwt.Claims)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Header["Token"] != nil {
+			token, err := jwt.Parse(r.Header["Token"][0], func(t *jwt.Token) (interface{}, error) {
+				_, ok := t.Method.(*jwt.SigningMethodHMAC)
+				if !ok {
+					w.WriteHeader(http.StatusUnauthorized)
+					w.Write([]byte("not authorized"))
+				}
+				return SECRET, nil
+			})
+
+			if err != nil {
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte("not authorized: " + err.Error()))
+			}
+
+			if token.Valid {
+				next(w, r, token.Claims)
+			}
+		} else {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("not authorized"))
+		}
+	})
+}
+
+func GetJwt(w http.ResponseWriter, r *http.Request) {
+	if r.Header["Access"] != nil {
+		if r.Header["Access"][0] != api_key {
+			return
+		} else {
+			token, err := CreateJWT()
+			if err != nil {
+				return
+			}
+			fmt.Fprint(w, token)
+		}
+	}
+}
+
+func Home(w http.ResponseWriter, r *http.Request, t jwt.Claims) {
+	fmt.Println(t)
+	fmt.Fprintf(w, "super secret area")
+}
+
+func main() {
+
+	http.Handle("/api", ValidateJWT(Home))
+	http.HandleFunc("/jwt", GetJwt)
+
+	http.ListenAndServe(":3500", nil)
+
+}
